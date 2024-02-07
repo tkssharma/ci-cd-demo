caa7d2e7a603a5b3cf95a3fa4b0ca6a179587502
tkssharma <tarun.softengg@gmail.com>
2024-02-07 23:40:43 +0530

    init

---
+++ deploy/sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# setting up the node environment
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+
+echo "starting deployment process"
+
+# download new version of our application
+cd /home/ubuntu/url-shortner-nodejs
+git pull origin master
+
+# install all the dependencies
+echo "installing dependencies"
+npm install
+# build the application
+echo "building application"
+npm run build
+
+# run the application
+echo "starting the application"
+cd ~
+pm2 reload ecosystem.config.js
+
+echo "deployment process completed"
